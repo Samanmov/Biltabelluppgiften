48f32e0d9d441c5e11e4032d84fb2ab8637a0514
saman movaghar <saman_mov@yahoo.com>
2016-04-02 07:41:37 +0200

    new uppdatering of files

--- app.js
+++ app.js
@@ -1,26 +1,16 @@
 var app = angular.module('carCars', []);
 
 app.controller('myController', function($scope) {
-    $scope.one = true;
-	$scope.two = false;
-	
-	
 	$scope.cars = [
-	{License:"KD2315", Brand:"Benz", Model:"Mercedes", Year:"2015"},
-	{License:"MD9070", Brand:"Volvo", Model:"S60", Year:"2014"}
+	{
+		License:"KD2315",Brand:"Benz", Model:"Mercedes", Year:"2015", 
+		editLicense:false, editBrand:false, editModel:false,  editYear:false
+	},
+	{License:"MD9070",Brand:"Volvo", Model:"S60", Year:"2014",
+	 editLicense:false , editBrand:false, editModel:false,  editYear:false
+	}
 	
 	];
-	$scope.showTwo = function (){
-	  $scope.one = false;
-	  $scope.two = true; // now show this one
-	 
-	  }
-	
-	$scope.save = function (){
-	  $scope.one = true; // now show this one
-	  $scope.two = false; 
-	 
-	  }
 	
 });
 
