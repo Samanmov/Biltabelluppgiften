b25d952fa2abf501ba0fdf9fb64d65e3ea01e6d2
saman movaghar <saman_mov@yahoo.com>
2016-04-02 13:09:34 +0200

    add separate files

--- app.js
+++ app.js
@@ -1,16 +1,3 @@
 var app = angular.module('carCars', []);
 
-app.controller('myController', function($scope) {
-	$scope.cars = [
-	{
-		License:"KD2315",Brand:"Benz", Model:"Mercedes", Year:"2015", 
-		editLicense:false, editBrand:false, editModel:false,  editYear:false
-	},
-	{License:"MD9070",Brand:"Volvo", Model:"S60", Year:"2014",
-	 editLicense:false , editBrand:false, editModel:false,  editYear:false
-	}
-	
-	];
-	
-});
 
---
+++ controller.js
@@ -0,0 +1,12 @@
+angular.module('carCars').controller('myController', function($scope) {
+	$scope.cars = [
+	{
+		License:"KD2315",Brand:"Benz", Model:"Mercedes", Year:"2015", 
+		editLicense:false, editBrand:false, editModel:false,  editYear:false
+	},
+	{License:"MD9070",Brand:"Volvo", Model:"S60", Year:"2014",
+	 editLicense:false , editBrand:false, editModel:false,  editYear:false
+	}
+	
+	];	
+});
